ac1d4f8d97f7005ad925cfd1ef1b4d25553f8590
M.P. Barzine <barzine@gmail.com>
2017-09-28 05:54:00 +0100

    Update Matrix.R

--- R/Matrix.R
+++ R/Matrix.R
@@ -85,7 +85,7 @@ Make_matrix_plot <- function(Mat_data,Set_size_data, Main_bar_data, point_size, 
   }
   
   Matrix_plot <- (ggplot() 
-                  + theme(base_family = 'Linux Libertine Mono', panel.background = element_rect(fill = "white"),
+                  + theme(text=element_text('Linux Libertine Mono'), panel.background = element_rect(fill = "white"),
                           plot.margin=unit(c(-0.2,0.5,0.5,0.5), "lines"),
                           axis.text.x = element_blank(),
                           axis.ticks.x = element_blank(),
