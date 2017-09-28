bc0ec4eab97e645760cab74910076a5e4cfba35f
M.P. Barzine <barzine@gmail.com>
2017-09-28 04:20:29 +0100

    addition of theme_scatter

--- R/theme.R
+++ R/theme.R
@@ -15,7 +15,8 @@ theme_boxplots<- function(base_size = 11, base_family = "",
                       panel.border = element_blank(),
                       panel.grid.minor = element_blank(),
                       panel.grid.major = element_blank(),
-                      axis.title.x = element_blank()
+                      axis.title.x = element_blank(),
+                      complete = TRUE
                       )
 }
 
@@ -33,7 +34,8 @@ theme_Main_bar_plot<-function(base_size = 11, base_family = "",
                   plot.margin = unit(c(0.5,0.5,bottom_margin,0.5), "lines"), 
                   panel.border = element_blank(),
                   axis.title.y = element_text(vjust = -0.8, size = 8.3*y_axis_title_scale), 
-                  axis.text.y = element_text(vjust=0.3,size=7*y_axis_tick_label_scale)
+                  axis.text.y = element_text(vjust=0.3,size=7*y_axis_tick_label_scale),
+                  complete = TRUE
                 )
 }
 
@@ -55,7 +57,8 @@ theme_Matrix_plot<-function(base_size = 11, base_family = "",
                   axis.ticks.y = element_blank(),
                   axis.text.y = element_text(colour = "gray0", size = 7*name_size_scale, hjust = 0.4),
                   panel.grid.major = element_blank(), 
-                  panel.grid.minor = element_blank()
+                  panel.grid.minor = element_blank(),
+                  complete = TRUE
                 )
 }
 
@@ -80,7 +83,8 @@ theme_metadataHist<-function(base_size = 11, base_family = "",
                    axis.text.y = element_blank(),
                    axis.ticks.y = element_blank(),
                    panel.grid.minor = element_blank(),
-                   panel.grid.major = element_blank()
+                   panel.grid.major = element_blank(),
+                   complete = TRUE
                 )
 }
 
@@ -107,7 +111,8 @@ theme_metadataHeat<-function(base_size = 11, base_family = "",
                    axis.ticks.y = element_blank(),
                    axis.title.y = element_blank(),
                    axis.text.x = element_blank(),
-                   axis.ticks.x = element_blank()
+                   axis.ticks.x = element_blank(),
+                   complete = TRUE
                   )
 }
 
@@ -134,11 +139,12 @@ theme_metadataText<-function(base_size = 11, base_family = "",
                    axis.ticks.y = element_blank(),
                    axis.title.y = element_blank(),
                    axis.text.x = element_blank(),
-                   axis.ticks.x = element_blank()
+                   axis.ticks.x = element_blank(),
+                   complete = TRUE
                 )
   }
 
-theme_sizeBar<-<-function(base_size = 11, base_family = "",
+theme_sizeBar<-function(base_size = 11, base_family = "",
                      base_line_size = base_size / 22,
                      base_rect_size = base_size / 22) {
      theme_bw(
@@ -160,11 +166,51 @@ theme_sizeBar<-<-function(base_size = 11, base_family = "",
                   axis.text.y = element_blank(),
                   axis.ticks.y = element_blank(),
                   panel.grid.minor = element_blank(),
-                  panel.grid.major = element_blank()
+                  panel.grid.major = element_blank(),
+                  complete = TRUE
                   )
   }
 
 
+theme_histo<-function(base_size = 11, base_family = "",
+                     base_line_size = base_size / 22,
+                     base_rect_size = base_size / 22) {
+     theme_bw(
+                base_size = base_size,
+                base_family = base_family,
+                base_line_size = base_line_size,
+                base_rect_size = base_rect_size
+              ) %+replace%
+                theme(
+                  panel.background = element_rect(fill = "white"),
+                  plot.title = element_text(vjust = 1.5),
+                  panel.border = element_blank(),
+                  panel.grid.minor = element_blank(),
+                  panel.grid.major = element_blank(),
+                  axis.title.y = element_text(vjust = 1.3, size = 8.3),
+                  axis.title.x = element_text(size = 8.3),
+                  plot.margin=unit(c(0.5,0,0,1), "cm"),
+                  complete = TRUE
+                )
+  }
 
-
-
+theme_scatter<-function(base_size = 11, base_family = "",
+                     base_line_size = base_size / 22,
+                     base_rect_size = base_size / 22) {
+     theme_bw(
+                base_size = base_size,
+                base_family = base_family,
+                base_line_size = base_line_size,
+                base_rect_size = base_rect_size
+              ) %+replace%
+                theme(
+                  panel.background = element_rect(fill = "white"),
+                  plot.title = element_text(vjust = 1.3),
+                  panel.grid.minor = element_blank(),
+                  panel.grid.major = element_blank(),
+                  axis.title.y = element_text(vjust = 1.3, size = 8.3),
+                  axis.title.x = element_text(size = 8.3),
+                  plot.margin=unit(c(0.5,0,0,1), "cm"),
+                  complete=TRUE
+                 )
+  }
